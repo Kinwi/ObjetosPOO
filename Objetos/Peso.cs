fd263955f0b9c59fe6e5c29fec936fd72f0b727c
Raúl López Hernando <raullopezhernando@gmail.com>
2019-10-09 18:39:42 +0200

    Hasta el Satellite

---
+++ Objetos/Peso.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objetos
+{
+    class Peso
+    {
+
+
+        private double peso;
+        private string medida;
+
+
+        public Peso(double peso,string medida) {
+
+            this.peso = peso;
+            this.medida = medida;
+        }
+
+       
+        public double GetLibras(double peso) {
+            double cambioALibra = 2.20462;
+            return peso * cambioALibra;
+        }
+
+
+        public double GetLingotes(double peso)
+        {
+            double cambioALingote = 1.0;
+            
+            return peso * cambioALingote;
+        }
+
+        public int GetPeso(int peso,string medida)
+        {
+
+            return;
+        }
+
+        
+
+    }
+}
--- Objetos/Program.cs
+++ Objetos/Program.cs
@@ -6,38 +6,66 @@ namespace Objetos
     {
         static void Main(string[] args)
         {
-            Coche coche = new Coche("Ranault","Laguna","Neon",120,5);
-            Coche coche2 = new Coche();
+            //Coche coche = new Coche("Ranault","Laguna","Neon",120,5);
+            //Coche coche2 = new Coche();
 
-            coche2.SetMarca("Ferrari");
-            coche.Arrancar();
-            Console.WriteLine(coche.GetMarca());
+            //coche2.SetMarca("Ferrari");
+            //coche.Arrancar();
+            //Console.WriteLine(coche.GetMarca());
 
-            Finanzas finanzas = new Finanzas();
-            Finanzas finanzas2 = new Finanzas(0.8);
+            //Finanzas finanzas = new Finanzas();
+            //Finanzas finanzas2 = new Finanzas(0.8);
 
-            Console.WriteLine("$ 5 euros son:{finanzas.EurosToDolares(5))}dolares");
-            Console.WriteLine("$ 8 dolars son:{finanzas2.DolaresToEuros(5))}euros");
+            //Console.WriteLine("$ 5 euros son:{finanzas.EurosToDolares(5))}dolares");
+            //Console.WriteLine("$ 8 dolars son:{finanzas2.DolaresToEuros(5))}euros");
 
 
-            Numero numero = new Numero();
-            Numero numero1 = new Numero(5);
+            //Numero numero = new Numero();
+            //Numero numero1 = new Numero(5);
 
-            numero.Aniade();
-            Console.WriteLine(numero.GetNumero());
-            numero1.GetTriple();
-            //numero1.SetNumero(numero1.GetTriple());
-            numero1.GetDoble();
-            //numero1.SetNumero(numero1.GetDoble());
-            Console.WriteLine(numero1.GetNumero());
+            //numero.Aniade();
+            //Console.WriteLine(numero.GetNumero());
+            //numero1.GetTriple();
+            ////numero1.SetNumero(numero1.GetTriple());
+            //numero1.GetDoble();
+            ////numero1.SetNumero(numero1.GetDoble());
+            //Console.WriteLine(numero1.GetNumero());
 
+            //Satelite satelite2 = new Satelite();
             //Satelite satelite1 = new Satelite(5,6,8);
+
+
             //satelite1.VariaAltura(5);
-            //satelite1.EnOrbita();
-            //satelite1.VariaPosicion(-5,8);
-            
+            //satelite2.EnOrbita();
+            //satelite2.VariaPosicion(-5,8);
 
 
+            Console.WriteLine(" -----Conversor de Pesos--------");
+            Console.WriteLine();
+            Console.WriteLine("--------------MENU--------------------");
+            Console.WriteLine("1.Devuelve el Peso introducido a Libras");
+            Console.WriteLine("2.Devuelve el Peso introducido a Lingotes");
+            Console.WriteLine("3.Devuelve el Peso en la Medida que tu elijas");
+            Console.WriteLine("Introduce el numero del MENU para realizar la accion");
+            int caseSwitch = Convert.ToInt32(Console.ReadLine());
+
+            Peso peso = new Peso(peso);
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("1.Devuelve el Peso introducido a Libras"););
+                    
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
         }
+
+
+    }
     }
 }
